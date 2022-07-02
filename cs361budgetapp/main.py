1b33aff3ae3303ce9f05dd3f1b6043740316c802
luonga <aluong1996@Gmail.com>
2022-07-02 01:27:29 -0600

    Home page

--- cs361budgetapp/main.py
+++ cs361budgetapp/main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
  
 app = Flask(__name__)
  
 @app.route('/')
 def index():
-  return '<h1>I want to Deploy Flask to Heroku</h1>'
+  return render_template("home.html")

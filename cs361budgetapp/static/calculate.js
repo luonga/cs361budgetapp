33b9450feec52aeed2238fd4cc2f1c8bd92c5bf8
luonga <aluong1996@Gmail.com>
2022-07-05 22:50:08 -0600

    calculator

--- cs361budgetapp/static/calculate.js
+++ cs361budgetapp/static/calculate.js
@@ -88,17 +88,15 @@ function calcTotal() {
   document.getElementById('total').innerHTML = '$' + total;
 }
 
-
+// Functions for calculator
 function clearScreen() {
   document.getElementById("result").value = "";
 }
 
-// This function display values
 function display(value) {
   document.getElementById("result").value += value;
 }
 
-// This function evaluates the expression and returns result
 function calculate() {
   var p = document.getElementById("result").value;
   var q = eval(p);

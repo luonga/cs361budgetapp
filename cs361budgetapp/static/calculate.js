2af8ae2749f174ba1556465837c734ccb79beece
luonga <aluong1996@Gmail.com>
2022-07-05 22:45:41 -0600

    calculator

--- cs361budgetapp/static/calculate.js
+++ cs361budgetapp/static/calculate.js
@@ -87,3 +87,20 @@ function calcTotal() {
   var total = income - spending;
   document.getElementById('total').innerHTML = '$' + total;
 }
+
+
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+
+// This function display values
+function display(value) {
+  document.getElementById("result").value += value;
+}
+
+// This function evaluates the expression and returns result
+function calculate() {
+  var p = document.getElementById("result").value;
+  var q = eval(p);
+  document.getElementById("result").value = q;
+}
